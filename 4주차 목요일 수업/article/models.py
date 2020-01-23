be1d6fd0589db4a8e7eed281163b47aae44a89d9
sanghyun <bejimil@g.skku.edu>
2020-01-23 17:53:27 +0900

    4주차 목요일 수업

---
+++ 4주차 목요일 수업/article/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name='제목')
+    content = models.TextField(verbose_name='내용')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="작성자")
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+
+    content = models.CharField(max_length=200, verbose_name='댓글')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='작성자')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
